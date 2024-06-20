4b394b5afdb52178cafb6af8cc94b38549e150fc
Woen-Sug Choi <woensug.choi@gmail.com>
2024-06-21 08:57:32 +0900

    typo fix

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 // api/redirect.js
 
 module.exports = (req, res) => {
-    res.writeHead(301, { Location: 'https://chatgee.notion.site/86a629fdccf64af48a23f1408d7f519em' });
+    res.writeHead(301, { Location: 'https://chatgee.notion.site/86a629fdccf64af48a23f1408d7f519e' });
     res.end();
   };
